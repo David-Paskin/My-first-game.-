e9047e2e2a49817fbc4cf7b620b7403c1b00db7a
Fusion-LP-TechLab <59736353+Fusion-LP-TechLab@users.noreply.github.com>
2026-03-25 14:19:23 -0400

    i added lose screen.

--- Assets/timer.cs
+++ Assets/timer.cs
@@ -7,7 +7,10 @@ public class timer : MonoBehaviour
 {
 
     // Assign your TMP_Text or TextMeshProUGUI component in the Unity Inspector
-    public TMP_Text timeText; 
+    public TMP_Text timeText;
+    public TMP_Text scoreText; 
+    public GameObject loseScreen;
+    public GameObject winScreen;
     private float time = 15.0f;
     public int scoreValue;
 
@@ -15,6 +18,15 @@ public class timer : MonoBehaviour
     {
 
             time -= Time.deltaTime;
+            if (time<0){
+                if (int.Parse(scoreText.text)<10){
+                   loseScreen.SetActive(true);
+                }
+                else
+                {
+                    winScreen.SetActive(true);
+                }
+            }
 
         // Convert the float to a string and assign it to the text component
         // You can use string formatting to control the number of decimal places
