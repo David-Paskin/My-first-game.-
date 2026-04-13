84beda4bd0188cdfa2c7a7a11942e8a4472c2759
Fusion-LP-TechLab <59736353+Fusion-LP-TechLab@users.noreply.github.com>
2026-04-13 14:22:09 -0400

    time

--- Assets/timer.cs
+++ Assets/timer.cs
@@ -9,24 +9,27 @@ public class timer : MonoBehaviour
     // Assign your TMP_Text or TextMeshProUGUI component in the Unity Inspector
     public TMP_Text timeText;
     public TMP_Text scoreText; 
+    public TMP_Text timeLeft; 
     public GameObject loseScreen;
     public GameObject winScreen;
     private float time = 15.0f;
     public int scoreValue;
+    bool isDone=false;
 
     void Update()
     {
 
-            time -= Time.deltaTime;
-            if (time<0){
-                if (int.Parse(scoreText.text)<10){
-                   loseScreen.SetActive(true);
-                }
-                else
-                {
-                    winScreen.SetActive(true);
-                }
-            }
+        time -= Time.deltaTime;
+        if(double.Parse(scoreText.text)>=10 && !isDone){
+            isDone=true;
+            winScreen.SetActive(true);
+            timeLeft.text=$"Time left: {time:F2}";
+        }
+        else if (time<0 && double.Parse(scoreText.text)<10 && !isDone){
+            isDone=true;
+            loseScreen.SetActive(true);
+            
+        }
 
         // Convert the float to a string and assign it to the text component
         // You can use string formatting to control the number of decimal places
